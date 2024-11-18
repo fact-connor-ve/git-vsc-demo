0965726df2ba00dfaba50afb1628d3bc8fa5a02d
fact-connor-ve <connorv@factivity.com>
2024-11-18 10:03:09 -0500

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git-vsc-demo
+
+go 1.22.5
---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	intr "git-vsc-demo/src/internals"
+)
+
+func main() {
+	intr.Example("First Example")
+	intr.Example2("Second Example")
+	fmt.Println("Main Call")
+}
